8f317c1cc6de3d27a44147a2dd0631ce343dc0a9
oppi85 <SvenRocho@gmx.de>
2015-12-09 16:12:36 +0100

    change user class

--- src/main/java/de/rocho/shopinglistserver/persistance/User.java
+++ src/main/java/de/rocho/shopinglistserver/persistance/User.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
@@ -39,7 +40,7 @@ public class User implements Serializable {
     private List<Recepe> recepeList;
     
     @ManyToMany
-    @Column(nullable = true)
+    @JoinColumn(nullable = true)
     private List<ShoppingList> shoppingLists;
     
     /*
