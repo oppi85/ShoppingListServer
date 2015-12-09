633429e24d4c12765fbefc82528a1a9b813a4a2b
oppi85 <SvenRocho@gmx.de>
2015-12-09 16:10:36 +0100

    change user class

--- src/main/java/de/rocho/shopinglistserver/persistance/User.java
+++ src/main/java/de/rocho/shopinglistserver/persistance/User.java
@@ -27,19 +27,19 @@ public class User implements Serializable {
     private Long id;
 
     @OneToOne
-    @Basic(optional=true)
+    @Column(nullable = true)
     private Store store;
     
     @OneToMany(mappedBy = "user")
-    @Basic(optional=true)
+    @Column(nullable = true)
     private List<ListEntry> listEntrys;
 
     @OneToMany(mappedBy = "user")
-    @Basic(optional=true)
+    @Column(nullable = true)
     private List<Recepe> recepeList;
     
     @ManyToMany
-    @Basic(optional=true)
+    @Column(nullable = true)
     private List<ShoppingList> shoppingLists;
     
     /*
